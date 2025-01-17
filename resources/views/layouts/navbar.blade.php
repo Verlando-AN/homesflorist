991b6c0dd6f0d8674996c61d96858a3d479cdb7e
Verlando77 <121385167+Verlando-AN@users.noreply.github.com>
2025-01-18 04:26:06 +0700

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -39,6 +39,7 @@
     </div>
 </nav>
 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoafP2VUjoV6Oq9DhjPrJwV3KSkv6ALQRIDHBpdPUIskzdf" crossorigin="anonymous"></script>
 
 <style>
 img.rounded-circle {
