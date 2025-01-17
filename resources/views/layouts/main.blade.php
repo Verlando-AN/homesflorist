939c4f1ca5e4d19d66bf25f234e5389520e731f1
Verlando77 <121385167+Verlando-AN@users.noreply.github.com>
2025-01-18 04:09:16 +0700

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-TN2N27yshwE6P2IHpCsMa9bQgFBdT5w+Zn/8LW4G65w9I9m/Vs9r3YO6UyCQc/x8" crossorigin="anonymous">
+
     <style>
         html, body {
             height: 100%; 
@@ -40,6 +41,7 @@
         </div>
     </div>
     @include('layouts.footer')
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-p2Cp3oLMZbd1JGFq91rm6E3/qfqcPbppNtFtyA9ViIgjOAGb1wxREYz24jz5zSVN" crossorigin="anonymous"></script>
+
 </body>
 </html>
